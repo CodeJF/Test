a9799fbd6e48e27547a16e66901c7c08f079c157
xjf@naratech.cn <xjf@naratech.cn>
2020-05-26 13:41:46 +0800

    a init

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by apple on 2020/5/26.
+//  Copyright © 2020 Space. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Test/Test/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by apple on 2020/5/26.
+//  Copyright © 2020 Space. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"helloworld");
+}
+
+
+@end
